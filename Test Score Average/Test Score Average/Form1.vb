eebfedec121982b179843c019ea11b3016a9fafd
tylerconger5 <33033554+tylerconger5@users.noreply.github.com>
2017-12-07 10:46:23 -0600

    Upodate

---
+++ Test Score Average/Test Score Average/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub btnScores_Click(sender As Object, e As EventArgs) Handles btnScores.Click
+        Dim intNumScores As Integer
+        Dim dblTestScores As Double
+        Dim dblTotal As Double
+        Dim dblTotal As 
+    End Sub
+End Class
---
+++ running total/running total/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("running total")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Davenport Community School District")> 
+<Assembly: AssemblyProduct("running total")> 
+<Assembly: AssemblyCopyright("Copyright © Davenport Community School District 2017")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("362d4b18-eb3b-4f6d-829b-bd137f17616b")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
